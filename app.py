805e86a648b512df9dc7ed064f6e5384347df8f2
Ahmed Mohamed <am811132@gmail.com>
2024-09-25 12:24:39 +0300

    Modified app.py

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 import requests
 import sys
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout)
+from PyQt5.QtCore import Qt
+
+class WeatherApp(QWidget):
+    def __init__(self):
+        super().__init__()
 
 
-url = requests.get("xx")
+def main():
+    app = QApplication(sys.argv)
+    weather_app = WeatherApp()
+    weather_app.show()
+    sys.exit(app.exec_())
+
+    
+if __name__ == "__main__":
+    main()
